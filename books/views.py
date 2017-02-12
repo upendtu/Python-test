3f8142fa638688ff0d91f29969377bd6ff4f3538
upendra <upendtu@gmail.com>
2017-02-12 21:16:38 +0900

    My django girls app, first commit

---
+++ books/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+from books.models import Books
+from django.template import Context, loader
+
+
+def index(request):
+	books_list = Books.objects.all()
+	t = loader.get_template('books/index.html')
+	c = Context({'books_list': books_list,})
+	return HttpResponse(t.render(c))
